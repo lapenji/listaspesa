8a8fb72a84328aa4d44ce7b75d4cd38b901cea6c
lapenji <lapenji@MacBook-Pro-di-Lapenji.local>
2020-05-15 16:42:16 +0200

    Updates

---
+++ precache-manifest.75a6899245d06b2444be7daf6b54e840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eae64a635b55da1c2104fd65d52a421",
+    "url": "/listaspesa/index.html"
+  },
+  {
+    "revision": "bddb0a736667df18f74f",
+    "url": "/listaspesa/static/css/main.d9853e01.chunk.css"
+  },
+  {
+    "revision": "f7d1aff3c1bab9e782ce",
+    "url": "/listaspesa/static/js/2.1360c205.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/listaspesa/static/js/2.1360c205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bddb0a736667df18f74f",
+    "url": "/listaspesa/static/js/main.20269e33.chunk.js"
+  },
+  {
+    "revision": "bc0e6fbf03ea097557f2",
+    "url": "/listaspesa/static/js/runtime-main.b194e492.js"
+  }
+]);
